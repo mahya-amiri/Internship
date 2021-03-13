d52508e28fefcd692ddc884597d110d5c7d607e9
Mahya <7mahya.amiri@gmail.com>
2021-03-13 12:21:09 +0330

    updated

--- src/React/counter-app/src/components/navbar.jsx
+++ src/React/counter-app/src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
-const NavBar = ({ totalCounters }) => {
+// sfc (Stateless Functional Component)
+const NavBar = (props) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {totalCounters}
+          {props.totalCounters}
         </span>
       </a>
     </nav>
