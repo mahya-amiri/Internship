681736f3b64e97fbcf56cb009d37fa96f82878d6
Mahya <7mahya.amiri@gmail.com>
2021-03-18 17:22:51 +0330

    uploaded

--- src/React/mahallat/src/components/family.jsx
+++ src/React/mahallat/src/components/family.jsx
@@ -3,19 +3,24 @@ import React, { Component } from "react";
 class Family extends Component {
   render() {
     return (
-      <form onSubmit={this.state.handleSubmit} className="float-right">
-        <label>
-          :شخص مورد نظر را انتخاب کنید
-          <select value={this.state.value} onChange={this.handleDropdown}>
-            <option value="Ali">علی امیری</option>
-            <option value="Masume">معصومه بنی اسد</option>
-            <option value="Mahsa">مهسا امیری</option>
+      <form>
+        <div className="form-group">
+          <label for="exampleFormControlInput1">
+            شخص مورد نظر را انتخاب کنید
+          </label>
+          <select
+            className="form-control"
+            id="exampleFormControlSelect1"
+            onChange={this.props.handleDropdown}
+          >
+            <option value="Father">علی امیری</option>
+            <option value="Mother">معصومه بنی اسد</option>
+            <option value="Sister">مهسا امیری</option>
             <option value="Ahmad">احمد بنی اسد</option>
             <option value="Leyla">لیلا شاهرخی</option>
             <option value="Smain">ثمین بنی اسد</option>
           </select>
-        </label>
-        <input type="submit" value="Submit" />
+        </div>
       </form>
     );
   }
