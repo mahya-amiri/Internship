5cfa2a25a2b9013fdb59ed72ba10d6041bed50e8
Mahya <7mahya.amiri@gmail.com>
2021-04-01 13:23:01 +0430

    updated

---
+++ src/React/vidly/src/components/common/listGroup.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props; //Object Distructuring
+  return (
+    <ul class="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperty]} className="list-group-item">
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id",
+};
+
+export default ListGroup;
